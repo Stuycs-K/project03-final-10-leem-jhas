a9606756c5ecb2f380dbb746b896d1ed42c884a0
Shwetlana Jha <sjha40@stuy.edu>
2024-01-10 23:13:09 -0500

    shared memory for victory, check if victory, generate random codewords

--- server.c
+++ server.c
@@ -8,21 +8,38 @@ static void sighandler(int signo) {
       exit(1);
     }
 }
+
+char* random_code_word(){
+    char list[5][50] = {"orange", "banana", "kiwi", "apple", "pineapple"};
+    srand(time(NULL));
+    int num = (rand() % (5));
+    char *output = list[num];
+    return output;
+}
 int main() {
     signal(SIGINT,sighandler);
 
-    char *code_word = "hello its me";
-    //shared memory 
+    char *code_word;
+    strcpy(code_word, random_code_word());
+    printf("code: %s\n", code_word);
+    code_word = "orange";
+    //shared memory for rounds
 
     int *data;
     int shmid;
     shmid = shmget(123, sizeof(int), IPC_CREAT | 0640);
     data = shmat(shmid, 0, 0);
     *data = 0;
-    printf("Round: %d\n", *data);
+    // printf("Round: %d\n", *data);
     shmdt(data); //detach
 
-
+    //  shared memory for victory 
+    int *data1;
+    int shmid1;
+    shmid1 = shmget(124, sizeof(int), IPC_CREAT | 0640);
+    data1 = shmat(shmid1, 0, 0);  
+    *data1 = 0;
+    shmdt(data1); //detach
     
     int w_file;
 
@@ -33,7 +50,8 @@ int main() {
     char modified_word[50];
     strcpy(modified_word,process(code_word));
     write(w_file,modified_word, 50);
-    printf("wrote %s\n", modified_word);
+    // printf("wrote %s\n", modified_word);
+    int victory;
     while(1){
         int to_client;
         int from_client;
@@ -46,7 +64,20 @@ int main() {
             exit(1);
         }
         else if(p == 0){ 
+            
+            int *data2;
+            int shmid2;
+            shmid2 = shmget(124, sizeof(int), IPC_CREAT | 0640);
+            data2 = shmat(shmid2, 0, 0);  
+            victory = *data2;
+            shmdt(data2); //detach
+            
             to_client = server_connect(from_client);
+            // printf("done\n");
+             //checks shared memory for victory 
+            if(victory>0){
+                printf("You won!!");
+            } 
             close(to_client);
             close(from_client);
             return p;
@@ -55,4 +86,6 @@ int main() {
     //remove shared memory 
     shmid = shmget(123, sizeof(int), IPC_CREAT | 0640);
     shmctl(shmid, IPC_RMID, 0); //remove the segment
+    shmid1 = shmget(124, sizeof(int), IPC_CREAT | 0640);
+    shmctl(shmid1, IPC_RMID, 0); //remove the segment
 } 
